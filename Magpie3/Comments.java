14d22e2d3d929149422d6978338887bcf6d81577
yamanakameko <inpq@fresnou.org>
2017-10-09 14:05:06 -0700

    ACT3 started

---
+++ Magpie3/Comments.java
@@ -0,0 +1,12 @@
+
+/**
+ * Write a description of class Comments here.
+ * 
+ * @author Kameko Yamanaka
+ * @version 10.9.17
+ * 
+ */
+public class Comments
+{
+    // Answers to ACT3 written here.
+}
